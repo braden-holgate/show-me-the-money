d45d841e2fca1ce242b3430bc21c9ccbb3ca99fc
Kelly Keating <keating.kel@gmail.com>
2022-04-26 18:11:01 +1200

    remove default route

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -40,8 +40,6 @@ function App () {
 
           {auth.isAuthenticated && <Route path="/meeting" element={<Meeting />} />}
           {auth.isAuthenticated && <Route path="/history" element={<History />} />}
-
-          <Route path="*" element={<Navigate to='/' />} />
         </Routes>
       </div>
 
